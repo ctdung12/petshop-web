f87357b7cb6abc0308f4b3701d28ae6cdc6989af
Ngoc Thinh Ngoc <thinhbaonhai@gmail.com>
2025-05-25 23:42:32 +0700

    pushapi2

--- petshop-web/frontend/resources/script/dogscript.js
+++ petshop-web/frontend/resources/script/dogscript.js
@@ -1,24 +1,3 @@
-// fetch("http://localhost:8080/api/product-dog")
-//     .then((Response) => Response.json())
-//     .then((data) =>  {
-//         const product = document.querySelectorAll(".productlist1child");
-//         product.forEach((el, index) => {
-//             const productdog = data[index];
-//             if(productdog) {
-//                 const nameEl = el.querySelector('#name');
-//                 const priceEl = el.querySelector('#price');
-//                 const brandEl = el.querySelector('#brand');
-
-//                 nameEl.textContent = productdog.name;
-//                 priceEl.textContent = ` Gia: ${productdog.price.toLocaleString('vi-VN')} d`;
-//                 brandEl.textContent = `${productdog.brand}`;
-//             }
-//         });
-//     })    
-//     .catch(error => {
-//         console.error('Error:', error);
-// });
-
 fetch("http://localhost:8080/api/product-dog")
     .then((Response) => Response.json())
     .then((data) => {
@@ -30,13 +9,13 @@ fetch("http://localhost:8080/api/product-dog")
             clone.style.display = "flex";
             clone.removeAttribute("id");
 
-            clone.querySeletor(".name").textConten = product.name;
-            clone.querySeletor(".price").textConten = product.price;
-            clone.querySeletor(".brand").textConten = product.brand;
+            clone.querySelector(".name").textContent = product.name || "Tên sản phẩm";
+            clone.querySelector(".price").textContent = product.price || " Chưa cập nhật";
+            clone.querySelector(".brand").textContent = product.brand || "Thương hiệu";
 
             productlist.appendChild(clone);
         });
     })
     .catch((error) => {
-        confirm.error("Loi",error);
-    });
+        console.error("Loi",error);
+});
